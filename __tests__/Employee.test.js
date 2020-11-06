56969fd03b61590ca5792e2b355c51fa8e36ac54
MarynaPR <pryadkamaryna@gmail.com>
2020-11-06 15:23:52 -0600

    manager created and tested

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,52 +1,40 @@
 
 const Employee = require('../lib/Employee.js');
 
-test('creates an employee object', () => {
-    const employee = new Employee(); //('Name', 1234, 'email@email.com');
-    expect(employee).toBe('object');
+// test('creates an employee object', () => {
+//     const employee = new Employee(); //('Name', 1234, 'email@email.com');
+//     expect(employee.employee).toBe('object');
 
-});
-
+// });
+//get value from constructor(name, id, email)
 test('create employee name', () => {
     const name = "AnyName";
     const employee = new Employee(name);
     expect(employee.name).toBe(name);
 });
-
 test('create employee id', () => {
     const value = 0;
     const employee = new Employee(name, value);
     expect(employee.id).toBe(value);
 });
-
 test('create employee email', () => {
     const value = 'email@email.com';
     const employee = new Employee(name, 0, value);
     expect(employee.email).toBe(value);
 });
-
-// test('get an ID for employee as an object'), () => {
-
-//     expect(employee.id).toHaveProperty('id');
-// };
-
-
-
-// expect(employee.email).toBe(email);
-// expect(employee.id).toBe(id);
-// expect(employee.email).toBe(email);
-
-//     expect(employee.name).toEqual(expect.any(String));
-//     expect(employee.name.length).toBeGreaterThan(0);
-//     expect(employee.id).toEqual(expect.any(Number));
-// });
-
-// The first class is an Employee parent class with the following properties and methods:
-
-
-// * `id`
-// * `email`
-// * `getName()`
-// * `getId()`
-// * `getEmail()`
-// * `getRole()`   // Returns 'Employee'
+//test for methods // * `getName()` * `getId()` * `getEmail()` * `getRole()`=>// Returns 'Employee'
+test('getName()', () => {
+    const value = "AnyName";
+    const employee = new Employee(value);
+    expect(employee.getName()).toBe(value);
+});
+test('getId()', () => {
+    const value = 0;
+    const employee = new Employee(name, value);
+    expect(employee.getId()).toBe(value);
+});
+test('getEmail()', () => {
+    const value = 'email@email.com';
+    const employee = new Employee(name, 0, value);
+    expect(employee.getEmail()).toBe(value);
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,6 +1,16 @@
+//const { TestScheduler } = require('jest');
+const Employee = require('../lib/Employee.js');
+const Manager = require('../lib/Manager.js');
 
-// In addition to `Employee`'s properties and methods, `Manager` will also have:
+//get value from constructor(officeNumber)
+test('create manager officeNumber', () => {
+    const value = 0;
+    const employee = new Manager('AnyName', 0, 'email@email.com', value);
+    expect(employee.officeNumber).toBe(value);
+});
 
-// * `officeNumber`
-
-// * `getRole()`   // Overridden to return 'Manager'
+test(`getRole()`, () => {
+    const value = 'Manager';
+    const employee = new Manager('AnyName', 0, 'email@email.com', 0);
+    expect(employee.getRole()).toBe(value);
+}); 
