247c8eba685aedfc65072515cb0152bbc7e6b203
MarynaPR <pryadkamaryna@gmail.com>
2020-11-08 18:46:08 -0600

    new code created in index and page-template with html creating the input

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
 # Team-Profile-Generator
 
-
-# 10 Object-Oriented Programming: Team Profile Generator
-
-
-
 Because this application won’t be deployed, you’ll also need to provide a link to a walkthrough video that demonstrates its functionality and all of the tests passing. You’ll need to submit a link to the video **and** add it to the README of your project.
 AS A manager
 I WANT to generate a webpage that displays my team's basic info
@@ -28,72 +23,6 @@ THEN I am prompted to enter the intern’s name, ID, email, and school, and I am
 WHEN I decide to finish building my team
 THEN I exit the application, and the HTML is generated
 
-### Mock-Up
-
-The following image shows the generated HTML’s appearance and functionality. The styling in the image is just an example, so feel free to add your own styles:
-
-![team profile generator demo](./Assets/10-object-oriented-programming-homework-demo.png)
-
-
-## Getting Started
-
-This Challenge will combine many of the skills you’ve learned over the first ten weeks of this course. In order to help you get started, we’ve provided some guidelines in addition to the User Story and Acceptance Criteria.
-
-Your application should use [Jest](https://www.npmjs.com/package/jest) for running the unit tests and [Inquirer](https://www.npmjs.com/package/inquirer) for collecting input from the user. The application will be invoked by using the following command:
-
-```
-node index.js
-```
-
-It is recommended that you start with a directory structure that looks like this:
-
-```md
-__tests__/			    // jest tests
-  Employee.test.js
-  Engineer.test.js
-  Intern.test.js
-  Manager.test.js
-dist/               // rendered output (HTML) and CSS style sheet
-lib/                // classes
-src/                // template helper code
-index.js            // Runs the application
-```
-
-The application must have these classes: `Employee`, `Manager`, `Engineer`, and `Intern`. The tests for these classes (in the tests directory) must all pass.
-
-The first class is an Employee parent class with the following properties and methods:
-
-* `name`
-* `id`
-* `email`
-* `getName()`
-* `getId()`
-* `getEmail()`
-* `getRole()`   // Returns 'Employee'
-
-The other three classes will extend `Employee`.
-
-In addition to `Employee`'s properties and methods, `Manager` will also have:
-
-* `officeNumber`
-
-* `getRole()`   // Overridden to return 'Manager'
-
-In addition to `Employee`'s properties and methods, `Engineer` will also have:
-
-* `github`  // GitHub username
-
-* `getGithub()`
-
-* `getRole()`   // Overridden to return 'Engineer'
-
-In addition to `Employee`'s properties and methods, `Intern` will also have:
-
-* `school`
-
-* `getSchool()`
-
-* `getRole()`   // Overridden to return 'Intern'
 
 Finally, although it’s not a requirement, you should consider adding validation to ensure that user input provided is in the proper expected format.
 
@@ -108,17 +37,17 @@ You are required to submit the following for review:
 
 * The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
 
-- - -
-© 2020 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+
 
 ///////////
 # Team-Profile-Generator
-
+The following image shows the generated HTML’s appearance and functionality.
 ![Screenshot](images/Screenshot.....png)
 
  ## Description
 
  This code showcases a Node.js command-line application that takes in information about employees and generates an HTML webpage that displays summaries for each person in a software engineering team. It provides well-crafted testing for each part of the code and ensures that is passes all unit tests.
+ Your application uses [Jest](https://www.npmjs.com/package/jest) for running the unit tests and [Inquirer](https://www.npmjs.com/package/inquirer) for collecting input from the user. 
  
  ## Technology Used
 
--- src/page-template.js
+++ src/page-template.js
@@ -1,154 +1,98 @@
-// create the about MY Team section
-const Employee = require('../lib/Employee');
-const Manager = require('../lib/Manager');
-const Intern = require('../lib/Intern');
-const Engineer = require('../lib/Engineer');
+// // create the about MY Team section
+// const Employee = require('../lib/Employee');
+// const Manager = require('../lib/Manager');
+// const Intern = require('../lib/Intern');
+// const Engineer = require('../lib/Engineer');
 
-function generateAllEmployees(dataArr) {
-  console.log("line8" + JSON.stringify(dataArr))
-  dataArr.manager.forEach((item) => {
-    //for (const data in dataArr.manager) {
-    generateEmployees(item)
-    console.log('GENERATE')
-  })
-  return ' '
-};
-
-function generateEmployees(data) {
-  console.log("data: " + data)
-  const { manager } = data
-  //console.log("managerARR" + manager)
-  //data(({ name, id, email }) => {
+//create employee section
+function generateTeam(data) {
   return `
-    <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${data.name}</h2>
-        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${data.employee}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${data.id}</li>
-            <li class="list-group-item">Email: <a href="mailto:${data.email}}">${data.email}</a></li>
-            <li class="list-group-item">Office number: ${data.office}</li>
-        </ul>
-    </div>
-</div>
-
-  `
-  //(data.manager => generateEmployees(manager))
-
-  //if (data === 'Manager') {
-  //const mng = new Manager(data.name, data.id, data.email, data.officeNumber)
-  //console.log(dataArr)
-  //console.log(data)
-
-
-
-  //   <div class="card employee-card">
-  //   <div class="card-header">
-  //       <h2 class="card-title">${manager.getName()}</h2>
-  //       <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
-  //   </div>
-  //   <div class="card-body">
-  //       <ul class="list-group">
-  //           <li class="list-group-item">ID: ${manager.getId()}</li>
-  //           <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}}">${manager.getEmail()}</a></li>
-  //           <li class="list-group-item">Office number: ${manager.getOfficeNumber()}}</li>
-  //       </ul>
-  //   </div>
-  // </div>
-  // `
-  //   }
-  //   const html = [];
-
-  //   html.push
-  //     .filter(employee => employee.setRole() === "Manager")
-
-  //manager.map(manager => generateEmployees(manager))
-
-  //   return html.join('');
-
-  //   //   <div class="card employee-card">
-  //   //   ${data
-  //   //         .filter(({ feature }) => feature)
-  //   //         .map(({ name, id, email }) => {
-  //   // //           return `
-
-  //   //       < div class="card-header" >
-  //   //       <h2 class="card-title">${data.name}</h2>
-  //   //       <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole}</h3>
-  //   //     </div >
-  //   //       <div class="card-body">
-  //   //         <ul class="list-group">
-  //   //           <li class="list-group-item">ID: ${manager.getId}</li>
-  //   //           <li class="list-group-item">Email: <a href="mailto:${manager.getEmail}}">${manager.getEmail}</a></li>
-  //   //           <li class="list-group-item">Office number: ${manager.getOfficeNumber}}</li>
-  //   //         </ul>
-  //   //       </div>
-  //   //   </div >
-  //   //   `
-  //   //         })
-  //   //         .join('')}
-  //   //   `;
-
-  //   // const page = [];
-
-  //   // page.push(dataArr
-  //   //   .filter(employee => employee.getRole() === "Manager")
-  //   //   .map(manager => generateEmployees(manager))
-  //   // );
-  //   // return page.join('');
-  // }
+        <section class="my-3" id="portfolio">
+          <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+          <div class="flex-row justify-space-between">
+          ${data
+      .filter(({ feature }) => feature)
+      .map(({ name, id, email, office }) => {
+        return `
+              <div class="col-12 mb-2 bg-dark text-light p-3">
+                <h3 class="portfolio-item-title text-light">${name}</h3>
+                <h5 class="portfolio-languages">
+                  Built With:
+         
+                </h5>
+                <p>${id}</p>
+                <a href="${office}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+              </div>
+            `;
+      })
+      .join('')}
+    
+          ${data
+      .filter(({ feature }) => !feature)
+      .map(({ name, id, email }) => {
+        console.log(id);
+        return `
+              <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+                <h3 class="portfolio-item-title text-light">${name}</h3>
+                <h5 class="portfolio-languages">
+                  Built With:
+                  ${id.join(', ')}
+                </h5>
+                <p>${office}</p>
+                <a href="${email}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+              </div>
+            `;
+      })
+      .join('')}
+        
+          </div>
+        </section>
+      `;
 };
-
-
 
 // export function to generate entire page
-module.exports = {
-  generate: (templateData) => {
-    console.log("PAGE")
+module.exports = templateData => {
+  //console.log("PAGE")
 
-    const { manager, intern, engineer } = templateData;
+  const { manager, intern, engineer } = templateData;
 
-    return `
-     <!DOCTYPE html >
-     <html lang="en">
-     <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     <title>Team-Page-Generator</title>
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-     <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-     <link rel="stylesheet" href="style.css">
+  return `
+    < !DOCTYPE html >
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <title>Team-Page-Generator</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+                  <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+                    <link rel="stylesheet" href="style.css">
      </head>
 
-   <body>
-   <div class="container-fluid">
-   <div class="row">
-   <div class="col-12 jumbotron mb-3 team-heading">
-   <h2 class="text-center">My Team</h2>
-   </div>
-   </div>
-   </div>
-   <div class="container">
-   <div class="row">
-   <div class="team-area col-12 d-flex justify-content-center">
-   ${generateAllEmployees(templateData)}
-   ${generateEmployees(templateData)}
-   </div>
-   </div>
-   </div>
-   </body>
+                    <body>
+                      <div class="container-fluid">
+                        <div class="row">
+                          <div class="col-12 jumbotron mb-3 team-heading">
+                            <h2 class="text-center">My Team</h2>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="container">
+                        <div class="row">
+                          <div class="team-area col-12 d-flex justify-content-center">
+                            ${generateTeam(templateData)}
 
-   <footer class="container text-center py-3">
-   <h3 class="text-dark">&copy;2020 by Maryna Pryadka</h3></footer>
+                          </div>
+                        </div>
+                      </div>
+                    </body>
+
+                    <footer class="container text-center py-3">
+                      <h3 class="text-dark">&copy;2020 by Maryna Pryadka</h3></footer>
    </body>
    </html>
-  `
-  }
-}
-
+  `;
+};
 // ${JSON.stringify(templateData)}
 // ${templateData.forEach((e) => `<p>${templateData}`)}
 // ${generateEmployees(manager)}
